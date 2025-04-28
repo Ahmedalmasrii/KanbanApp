b8e437500df2583169fdd845e9beff31d7a35b2f
Ahmed Almasri <ahmedalmasri2013@gmail.com>
2025-04-28 11:26:47 +0200

    Konfigurerade Axios-instans med bas-URL och automatisk token-hantering

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Lägg till token automatiskt i alla requests
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default instance;
