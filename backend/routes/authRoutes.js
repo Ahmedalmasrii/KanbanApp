98538e104d847fe7441bb9df883c8e0cd3a35680
Ahmed Almasri <ahmedalmasri2013@gmail.com>
2025-05-21 11:31:21 +0200

    bytt ut : const router = require('express').Router(); mot const express = require('express'); const router = express.Router();

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const authCtrl = require('../controllers/authController');
 
 router.post('/register', authCtrl.register);
