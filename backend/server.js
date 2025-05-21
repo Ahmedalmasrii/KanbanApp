0a9a5159131bfc02725d4145a5a30dd79cebc60b
Ahmed Almasri <ahmedalmasri2013@gmail.com>
2025-05-21 09:01:59 +0200

    fixat till min server.js

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// ROUTER
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
+// MONGODB CONNECTION
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ MongoDB connected');
+    app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+  })
+  .catch(err => console.error(err));
