6561c0dcd216a83b29105ec6bd473c9953072daa
Ahmed Almasri <ahmedalmasri2013@gmail.com>
2025-05-26 21:21:00 +0200

    Formaterar min server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
-
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -10,18 +9,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// ROUTER
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/orders', require('./routes/orderRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
-
-const statsRoutes = require('./routes/statsRoutes');
-app.use('/api/stats', statsRoutes); // ✅ Lägg till detta
+// ROUTES
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/stats", require("./routes/statsRoutes"));
+app.use("/api/notifications", require("./routes/notificationRoutes"));
 
 // MONGODB CONNECTION
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('✅ MongoDB connected');
-    app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+    console.log("✅ MongoDB connected");
+    app.listen(5000, () =>
+      console.log("Server running on http://localhost:5000")
+    );
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
