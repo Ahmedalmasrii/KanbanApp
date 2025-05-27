ec7bf997555a41236c63bbd0b054e3c0b7a6a834
Ahmed Almasri <ahmedalmasri2013@gmail.com>
2025-05-27 14:41:29 +0200

    skapat en service worker

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'kanban-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/offline.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request).then(response => {
+      return response || caches.match('/offline.html');
+    }))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
+  );
+});
