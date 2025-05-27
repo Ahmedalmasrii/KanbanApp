876cd7abecabdc24611a0f88700a9077b3d97a5b
Ahmed Almasri <ahmedalmasri2013@gmail.com>
2025-05-27 14:08:13 +0200

    deploy test

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,40 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+
+// dotenv.config();
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+
+// // ROUTES
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/orders", require("./routes/orderRoutes"));
+// app.use("/api/users", require("./routes/userRoutes"));
+// app.use("/api/stats", require("./routes/statsRoutes"));
+// app.use("/api/notifications", require("./routes/notificationRoutes"));
+// app.use("/api/activity", require("./routes/activityRoutes"));
+
+// // MONGODB CONNECTION
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("✅ MongoDB connected");
+//     app.listen(5000, () =>
+//       console.log("Server running on http://localhost:5000")
+//     );
+//   })
+//   .catch((err) => console.error(err));
+
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 
@@ -22,8 +55,10 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
-    app.listen(5000, () =>
-      console.log("Server running on http://localhost:5000")
-    );
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,26 @@
+// import axios from "axios";
+
+// const instance = axios.create({
+//   baseURL: "http://localhost:5000/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+
+// instance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default instance;
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://kanbanapp-u467.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
